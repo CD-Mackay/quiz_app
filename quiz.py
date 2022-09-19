5c936d36b14a6b9c2ac572e84e5d350ac94363b6
Connor Mackay <connor.mackay@gmail.com>
2022-09-19 17:07:14 -0400

    style quiz presentation

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+from string import ascii_lowercase
+
 QUESTIONS = {
   "When was the first use of the word 'quiz'": ["1781", "1771", "1871", "1881"],
   "What method can we use to get user input?": ["input", "get", "print", "write"],
@@ -6,14 +8,16 @@ QUESTIONS = {
   "To combine several strings into one", "To compress several files into one archive", "to get information from the user"]
 }
 
-for question, alternatives in QUESTIONS.items():
+for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+  print(f"\nQuestion {num}:")
+  print(f"{question}?")
   correct = alternatives[0]
-  sorted_alternatives = sorted(alternatives)
-  for label, alternative in enumerate(sorted_alternatives):
+  labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+  for label, alternative in labeled_alternatives.items():
       print(f" {label}) {alternative}")
 
-  answer_label = int(input(f"{question}?"))
-  answer = sorted_alternatives[answer_label]
+  answer_label = input("\nChoice?")
+  answer = labeled_alternatives.get(answer_label)
   if answer == correct:
     print("Nailed it!")
   else:
