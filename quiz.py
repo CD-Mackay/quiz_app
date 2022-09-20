396ba43d490b0938d0d937c2dce79f57149fd5f0
Connor Mackay <connor.mackay@gmail.com>
2022-09-20 19:14:52 -0400

    modularize functionality

--- quiz.py
+++ quiz.py
@@ -9,23 +9,68 @@ QUESTIONS = {
   "To combine several strings into one", "To compress several files into one archive", "to get information from the user"]
 }
 
-num_questions = min(NUM_PER_QUIZ, len(QUESTIONS))
-questions = random.sample(list(QUESTIONS.items()), k=num_questions)
-num_correct = 0
-for num, (question, alternatives) in enumerate(questions, start=1):
-  print(f"\nQuestion {num}:")
+##
+
+def run_quiz():
+  #Preprocess
+  questions = prepare_questions()
+
+  # Process (Main Loop)
+  num_correct = 0
+  for question in questions:
+    num_correct += ask_question(question)
+
+  # Postprocess
+  print(f"\nYou got {num_correct} correct!")
+
+## -------
+def prepare_questions():
+  num_questions = min(NUM_PER_QUIZ, len(QUESTIONS))
+  questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+  return questions
+
+## ------
+
+def get_answer(question, alternatives):
   print(f"{question}?")
-  correct = alternatives[0]
-  labeled_alternatives = dict(zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives))))
+  labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
   for label, alternative in labeled_alternatives.items():
-      print(f" {label}) {alternative}")
+    print(f" {label}) {alternative}")
 
   while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
     print(f"Please answer one of {', '.join(labeled_alternatives)}")
+  return labeled_alternatives[answer_label]
+
+## ------
+
+def ask_question(question, alternatives):
+  correct_answer = alternatives[0]
+  ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+   
   answer = labeled_alternatives.get(answer_label)
   if answer == correct:
     print("Nailed it!")
     num_correct += 1
   else:
     print(f"Apologies, the correct answer was {correct!r}, not {answer!r}")
+
+
+
+##
+# for num, (question, alternatives) in enumerate(questions, start=1):
+#   print(f"\nQuestion {num}:")
+#   print(f"{question}?")
+#   correct = alternatives[0]
+#   labeled_alternatives = dict(zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives))))
+#   for label, alternative in labeled_alternatives.items():
+#       print(f" {label}) {alternative}")
+
+#   while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+#     print(f"Please answer one of {', '.join(labeled_alternatives)}")
+#   answer = labeled_alternatives.get(answer_label)
+#   if answer == correct:
+#     print("Nailed it!")
+#     num_correct += 1
+#   else:
+#     print(f"Apologies, the correct answer was {correct!r}, not {answer!r}")
 print(f"\nYou got {num_correct} correct out of a total of {num}")
